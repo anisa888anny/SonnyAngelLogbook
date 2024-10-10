5bc8c232e45c3fa801b8f6e57958e4cc3f20e7f8
anisapirani <apiran03@student.ubc.ca>
2024-10-09 17:46:26 -0700

    implemented LoggerApp and Main.

---
+++ src/main/ui/LoggerApp.java
@@ -0,0 +1,93 @@
+package ui;
+
+import java.util.Scanner;
+import model.LogEntry;
+import model.Logbook;
+import java.util.List;
+
+public class LoggerApp {
+    private Logbook logbook;
+    private Scanner scanner;
+
+    public LoggerApp() {
+        logbook = new Logbook();
+        scanner = new Scanner(System.in);
+
+    }
+
+    // EFFECTS runs the console application
+    public void run() {
+        boolean keepRunning = true;
+
+        while (keepRunning) {
+            displayMenu();
+            String command = scanner.nextLine();
+
+            switch (command) {
+                case "1":
+                    addLogEntry();
+                    break;
+                case "2":
+                    viewAvailableAngels();
+                    break;
+                case "3":
+                    viewCollectionCount();
+                    break;
+                case "4":
+                    keepRunning = false;
+                    System.out.println("Exiting!");
+                    break;
+                default:
+                    System.out.println("Invalid, try again!");
+                    break;
+
+            }
+        }
+    }
+
+    // EFFECTS: displays the main menu and its options
+    private void displayMenu() {
+        System.out.println("Welcome to the Sonny Angel Logger!");
+        System.out.println("1. Add new log entry?");
+        System.out.println("2. View current Sonny Angel collection?");
+        System.out.println("3. Check how many Sonny Angels are in collection?");
+        System.out.println("4. Exit?");
+        System.out.println("Please choose a command: ");
+    }
+
+    // EFFECTS asks user to enter details for new log entry then adds to logbook
+    private void addLogEntry() {
+        System.out.println("Enter the name of your Sonny Angel: ");
+        String name = scanner.nextLine();
+
+        System.out.println("Enter the date of your transaction (YYYYMMDD): ");
+        int date = Integer.parseInt(scanner.nextLine());
+
+        System.out.println("Enter the transaction type (added, sold or traded): ");
+        String transactionType = scanner.nextLine();
+
+        LogEntry entry = new LogEntry(name, date, transactionType);
+        logbook.addLogEntry(entry);
+        System.out.println("Log entry added successfully!");
+
+    }
+
+    private void viewAvailableAngels() {
+        List<String> angels = logbook.getAvailableAngels();
+        if (angels.isEmpty()) {
+            System.out.println("No Sonny Angels here :(");
+        } else {
+            System.out.println("Available angels: ");
+            for (String angel : angels) {
+                System.out.println(angel);
+            }
+        }
+    }
+
+
+    // EFFECTS displays the number of collected sonny angels
+    private void viewCollectionCount() {
+        int count = logbook.getCollectionCount();
+        System.out.println("You have " + count + " Sonny Angels in your collection!");
+    }
+}
